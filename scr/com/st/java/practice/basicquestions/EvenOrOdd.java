5886afd0f9b6a82ba3c3380e74fae78ab86f02d6
prasannaganna <prasannaganna1216@gmail.com>
2026-03-10 16:02:18 +0530

    Day-1 Solved EvenOrOdd problem using if-else

---
+++ scr/com/st/java/practice/basicquestions/EvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.st.java.practice.basicquestions;
+//Given an integer n, determine whether the number is even or odd.
+// value of n is decleraed  in code not taking form user just using the if else staments
+public class EvenOrOdd {
+    static void main(String[] args) {
+        int n =12;
+        if(n%2==0)
+        {
+            System.out.println(n +":is even");
+        }
+        else
+        {
+            System.out.println(n + ": is odd");
+        }
+    }
+}
