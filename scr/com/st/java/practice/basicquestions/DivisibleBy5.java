cd66c4c7b342818606c0550cd7f06fabd5dbd146
prasannaganna <prasannaganna1216@gmail.com>
2026-03-10 21:08:18 +0530

    day 1 divisible by 5

---
+++ scr/com/st/java/practice/basicquestions/DivisibleBy5.java
@@ -0,0 +1,14 @@
+package com.st.java.practice.basicquestions;
+
+public class DivisibleBy5 {
+    static void main(String[] args) {
+        int n =10;
+        if(n%5==0){
+            System.out.println("divisible by 5");
+
+        }
+        else{
+            System.out.println("not divisible by 5");
+        }
+    }
+}
