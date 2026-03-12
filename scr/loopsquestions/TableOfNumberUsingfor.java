b734bb1cb371662437753beade57dfb5930e75f1
prasannaganna <prasannaganna1216@gmail.com>
2026-03-12 20:42:52 +0530

    day 3 table of the number

---
+++ scr/loopsquestions/TableOfNumberUsingfor.java
@@ -0,0 +1,17 @@
+package loopsquestions;
+
+import java.util.Scanner;
+
+public class TableOfNumberUsingfor {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number : " );
+        int n = sc.nextInt();
+        int result;
+        for(int i=1;i<=n;i++)
+        {
+            result = n*i;
+            System.out.println(n + "*" + i  + "=" + result);
+        }
+    }
+}
