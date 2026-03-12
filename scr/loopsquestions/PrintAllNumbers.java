62acd1990fe40e36707d99782832edcf1d329e6b
prasannaganna <prasannaganna1216@gmail.com>
2026-03-12 08:48:00 +0530

    day 3 solving the problems using loops

---
+++ scr/loopsquestions/PrintAllNumbers.java
@@ -0,0 +1,14 @@
+package loopsquestions;
+
+import java.util.Scanner;
+
+//Print numbers from 1 to n using for loop
+public class PrintAllNumbers {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+        for(int i =0; i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
