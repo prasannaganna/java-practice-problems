970fcd5ad6cbb126555d49dc579aeb7d0d663e79
prasannaganna <prasannaganna1216@gmail.com>
2026-03-12 18:36:18 +0530

    day 3 printing the numbers in reverse

---
+++ scr/loopsquestions/PrintReverseNTo1.java
@@ -0,0 +1,12 @@
+package loopsquestions;
+
+public class PrintReverseNTo1 {
+    static void main(String[] args) {
+        int n =5;
+        //int i;
+        for(int i=n;i>=0;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
