8dafe1ec9b6f1a78e0a0b7c85eb2c7f0665f8a3f
prasannaganna <prasannaganna1216@gmail.com>
2026-03-13 22:11:57 +0530

    initialization of the array

---
+++ scr/Arrays/AllNumbersInArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+//All NUMBERS IN THE ARRAY
+public class AllNumbersInArray {
+    static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6};
+        //int[] arr={1,2,3,4,5}// array decleration
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
