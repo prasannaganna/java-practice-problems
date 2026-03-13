3f74b1b3fed712339791900225fbd061881d90d9
prasannaganna <prasannaganna1216@gmail.com>
2026-03-13 22:23:37 +0530

    day 4 sum of digits in array

---
+++ scr/Arrays/SumOfElementsInArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+// sum of the  digits in the array
+public class SumOfElementsInArray {
+    static void main(String[] args) {
+        int[] array ={1,2,3,4,5,6};
+        int sum =0;
+        for(int i=0;i<array.length;i++){
+            sum =sum+array[i];
+        }
+        System.out.println(sum);
+    }
+}
